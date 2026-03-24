4a73ce29d6c8f1a6db8e38eaffe966944685b3a3
BenSterling29 <bensterling8324@gmail.com>
2026-03-24 14:52:29 -0400

    Add files via upload

---
+++ mysh.c
@@ -0,0 +1,14 @@
+// mysh - a simple Unix shell
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main(void) {
+    return 0;
+}
