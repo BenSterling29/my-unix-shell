6f33e4eca3f528388e80d72af05d2521c47999fe
Ben Sterling <bensterling@Bens-MacBook-Pro.local>
2026-03-26 14:14:04 -0400

    Update mysh.c from CLion

--- mysh.c
+++ mysh.c
@@ -10,5 +10,32 @@
 #include <signal.h>
 
 int main(void) {
+    char line[1024];
+
+    char *args[64];
+    int arg_count;
+
+    while (1) {
+        printf("mysh> ");
+        fflush(stdout);
+
+        if (fgets(line, sizeof(line), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        char *newline = strchr(line, '\n');
+        if (newline) *newline = '\0';
+
+        arg_count = 0;
+        args[arg_count] = strtok(line," ");
+
+        while (args[arg_count] != NULL) {
+            arg_count++;
+            args[arg_count] = strtok(NULL," ");
+        }
+
+        printf("You entered: %s\n", line);
+    }
     return 0;
 }
